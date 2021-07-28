c89f36855b6e93499d95284d478550f1a46fbe19
ethanmac <ethanmac@mac.com>
2021-07-28 21:08:09 +0800

    update

--- detect_exp.py
+++ detect_exp.py
@@ -23,8 +23,26 @@ from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, colorstr, non_max_suppression, \
     apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
-from utils.plots import colors, plot_one_box, plot_one_box_PIL
+from utils.plots import colors, plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_sync
+
+def plot_one_box_ex(xyxy, im0, cls, hide_labels, names, hide_conf, conf, line_thickness):
+    c = int(cls)  # integer class
+    #label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+
+    label = names[c] 
+    print(xyxy, label, conf)
+
+    names = label.split("-")
+    if len(names) == 2:
+        label = names[1]
+    if label.find("BK0") != -1:
+        label = None
+    elif label.find("hand") != -1:
+        label = None
+
+    if label is not None:
+        plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=line_thickness)
 
 
 @torch.no_grad()
@@ -147,10 +165,11 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
                     if save_img or save_crop or view_img:  # Add bbox to image
-                        c = int(cls)  # integer class
-                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                        #c = int(cls)  # integer class
+                        #label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
                         #plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=line_thickness)
-                        plot_one_box_PIL(xyxy, im0, label=label, color=colors(c, True), line_thickness=line_thickness)
+                        #plot_one_box_PIL(xyxy, im0, label=label, color=colors(c, True), line_thickness=line_thickness)
+                        plot_one_box_ex(xyxy, im0, cls, hide_labels, names, hide_conf, conf, line_thickness)
                         if save_crop:
                             save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
 
