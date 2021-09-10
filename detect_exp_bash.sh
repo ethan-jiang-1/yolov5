4faa759206011ddab6f0349dfe7d73ac1127624a
ethanmac <ethanmac@mac.com>
2021-09-10 23:03:54 +0800

    update

---
+++ detect_exp_bash.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#
+
+python detect.py --weights weights/yolov5s/run_sac60_r1_e300_model-best.pt --imgsz 640 --line-thickness 1 --save-txt --source ../ds_yolov5_exam/exam_sac15/images_0
